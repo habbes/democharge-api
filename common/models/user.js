a83a87c4c31a0baa8f461e578e5c6c732bfd7d94
Habbes <haby_habbes@outlook.com>
2016-10-13 20:44:58 +0300

    Create wallet when user is created

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,16 @@
 module.exports = function(User) {
 
+    /**
+     * hook to create wallet when user is created
+     * @author Habbes
+     * @added 13.10.2016
+     * @version 1
+     */
+    User.observe('before save', (context, next) => {
+        if(!context.isNewInstance){
+            return next();
+        }
+        let user = context.instance;
+        user.wallet.create(next);
+    });
 };
